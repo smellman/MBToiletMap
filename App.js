be97369ebfb5c3e9e1a78a7e1cadaf17279bf450
Taro Matsuzawa <taro@georepublic.co.jp>
2018-07-28 21:02:39 +0900

    コメントを追加

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
+// 1: MapboxGL をimportする
 import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
 export default class App extends Component<Props> {
 
+  // 2: componentWillMountでアクセストークンを指定する
   async componentWillMount () {
-    MapboxGL.setAccessToken("あなたのキー");
+    MapboxGL.setAccessToken("アクセストークン");
   }
 
   render() {
+    // 3: 地図を東京駅を中心に描画する
     return (
       <MapboxGL.MapView
         zoomLevel={14}
