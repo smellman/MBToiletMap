5c5ce07ea1b5b634baf963531c7fcc132f5aaae2
Taro Matsuzawa <taro@georepublic.co.jp>
2018-07-28 20:59:07 +0900

    MapboxGL Viewを表示

--- App.js
+++ App.js
@@ -1,49 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {View} from 'react-native';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
 export default class App extends Component<Props> {
+
+  async componentWillMount () {
+    MapboxGL.setAccessToken("あなたのキー");
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+      <MapboxGL.MapView
+        zoomLevel={14}
+        centerCoordinate={[139.766403, 35.681262]}
+        styleURL="mapbox://styles/mapbox/streets-v10"
+        style={{flex: 1}}
+      />
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
